c274d934a4d5f96a30c6bf2fa3240f73cbfebd4f
HeitorBMarini <mariniheitor@gmail.com>
2023-05-05 17:22:02 -0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import Formulario from './components/Formulário';
 
 
 function App() {
   return (
     <div className="App">
-      <h1>Olá</h1>
+      <Formulario/>
     </div>
   );
 }
